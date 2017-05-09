d73a70ff4ef91e5e25b5f9473b923dcc44aa502b
dltxiao <729457563@qq.com>
2017-05-09 15:05:31 +0800

    add user-agent config

--- downloader.py
+++ downloader.py
@@ -3,13 +3,21 @@
 
 import urllib2
 
-def download(url):
+user_agent = "zcom"
+
+def download(url, retry_num = 2):
     print "Downloading :", url
+    header = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=header)
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         print "download error: ", e.reason
         html = None
+        if retry_num > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                #遇到5xx错误时重新下载
+                return download(url, retry_num - 1)
     return html
 
-print download("http://www.baidu.com")
+print download("http://httpstat.us/500")
